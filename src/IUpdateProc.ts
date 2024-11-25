325c96774c07de3422361c82b3f65e01059e41bd
Feijian Ma <ma_feijian@163.com>
2024-11-25 11:11:45 +0800

    fix IUpdateProc

--- src/IUpdateProc.ts
+++ src/IUpdateProc.ts
@@ -1,8 +1,11 @@
 import { Arg } from './Arg';
+import { ErrorStatus } from './ErrorStatus';
 import { IRegenDoc } from './IRegenDoc';
 
 export interface IUpdateProc {
   getResultArg(): Arg;
   // eslint-disable-next-line no-unused-vars
   getInputArgs(doc: IRegenDoc): Arg[];
+  // eslint-disable-next-line no-unused-vars
+  execute(doc: IRegenDoc): ErrorStatus;
 }
